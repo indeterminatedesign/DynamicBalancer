6cdd9f32588bd5a7ec1b4297f31b4894cacd5de3
Indeterminate Design <61812044+indeterminatedesign@users.noreply.github.com>
2021-11-15 22:02:57 -0500

    Initial commit

---
+++ .pio/libdeps/esp32dev/TinyMPU6050/examples/PlatformIO_Angles_Example/src/PlatformIO_Angles_Example.cpp
@@ -0,0 +1,47 @@
+/*
+ *  Mandatory includes
+ */
+#include <Arduino.h>
+#include <TinyMPU6050.h>
+
+/*
+ *  Constructing MPU-6050
+ */
+MPU6050 mpu (Wire);
+
+/*
+ *  Setup
+ */
+void setup() {
+
+  // Initialization
+  mpu.Initialize();
+
+  // Calibration
+  Serial.begin(9600);
+  Serial.println("=====================================");
+  Serial.println("Starting calibration...");
+  mpu.Calibrate();
+  Serial.println("Calibration complete!");
+  Serial.println("Offsets:");
+  Serial.print("GyroX Offset = ");
+  Serial.println(mpu.GetGyroXOffset());
+  Serial.print("GyroY Offset = ");
+  Serial.println(mpu.GetGyroYOffset());
+  Serial.print("GyroZ Offset = ");
+  Serial.println(mpu.GetGyroZOffset());
+}
+
+/*
+ *  Loop
+ */
+void loop() {
+
+  mpu.Execute();
+  Serial.print("AngX = ");
+  Serial.print(mpu.GetAngX());
+  Serial.print("  /  AngY = ");
+  Serial.print(mpu.GetAngY());
+  Serial.print("  /  AngZ = ");
+  Serial.println(mpu.GetAngZ());
+}
---
+++ src/main.cpp
@@ -0,0 +1,314 @@
+#include <Arduino.h>
+#include <ESP32Servo.h>
+#include <MPU6050.h>
+
+Servo esc;
+
+volatile uint32_t pulseCount;
+volatile boolean rotationCompleteFlag = false;
+uint32_t rotationCount;
+boolean runComplete = false;
+
+uint32_t previousRPMControllerMicros;
+const uint32_t RPMControllerInterval = 100000;
+
+uint32_t previousRotationMicros = micros();
+uint32_t lastPulseMicros;
+uint16_t currentRPM;
+float averageTimePerPulseFromPreviousRevolution; //Using the total time required for the last revolution, divide by the number of poles to give an average duration per pole for estimating current position
+
+uint32_t previousDataOutputMicros;
+uint16_t dataInterval = 1000;
+
+
+boolean strobeLEDFlag = false;
+uint16_t strobeLEDDuration = 1000;
+uint32_t strobeLEDPreviousMicros = 0;
+
+uint32_t previousMPUMicros;
+uint16_t mpuInterval = 2000;
+
+const uint16_t targetRPM = 6000;
+
+float_t kP = 0.25;
+float_t kI = 0.0;
+float_t kD = 0.00;
+
+//Constants
+uint16_t zeroAngleOffset = 270;
+uint16_t numberOfPoles = 14;
+float_t degreesPerPole = 360 / numberOfPoles;
+#define ESCMax 2000
+#define ESCMin 1000
+#define ESCRangeLimiter 1200
+#define LEDPin 23
+#define HallPin 18
+#define ServoPin 19
+#define NumberOfRotationsPerRun 1000
+//#define Debug
+
+//Magnitude of Accel, Angle
+uint32_t runData[2][NumberOfRotationsPerRun];
+int32_t maxAccelPerRotation;
+int16_t maxAccelPosition;
+
+MPU6050 mpu;
+int16_t outputESC;
+
+void calcRPM();
+void IRAM_ATTR addPulse();
+uint16_t getPosition();
+float_t EMA_function(float alpha, float latest, float stored);
+void strobeLED();
+float_t processMPU();
+void controlRPM();
+void outputData();
+
+//Add a pulse every time the hall sensor changes state
+void IRAM_ATTR addPulse()
+{
+  pulseCount++;
+  lastPulseMicros = micros();
+
+  if (pulseCount % numberOfPoles == 0)
+  {
+    pulseCount = 0;
+    rotationCompleteFlag = true;
+    strobeLEDFlag = true;
+  }
+}
+
+void setup()
+{
+  Serial.begin(250000);
+  Serial.println("Started");
+  // put your setup code here, to run once:
+  pinMode(LEDPin, OUTPUT);
+  pinMode(HallPin, INPUT);
+
+  attachInterrupt(HallPin, addPulse, CHANGE);
+
+  //Initialize MPU
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+  mpu.initialize();
+  mpu.CalibrateAccel(6);
+
+  Serial.println("Testing device connections...");
+  Serial.println(mpu.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+  //DLPF Setting
+  mpu.setDLPFMode(MPU6050_DLPF_BW_98);
+  //mpu.CalibrateAccel(6);
+
+  //mpu.Calibrate();
+
+  esc.attach(ServoPin, 1000, 2000);
+
+  esc.writeMicroseconds(2000);
+  delay(3000);
+  esc.writeMicroseconds(1000);
+  delay(3000);
+}
+
+void loop()
+{
+ strobeLED();
+  if (!runComplete)
+  {
+     outputData();
+    if (rotationCompleteFlag)
+    {
+
+      runData[0][rotationCount] = maxAccelPerRotation;
+      runData[1][rotationCount] = maxAccelPosition;
+      maxAccelPerRotation = 0;
+      maxAccelPosition = 0;
+
+      rotationCount++;
+      //Serial.println("Rotation Complete");
+      //Based on when the last rotation started, store how long a rotation has taken
+      averageTimePerPulseFromPreviousRevolution = (micros() - previousRotationMicros) / (float)numberOfPoles;
+
+      calcRPM();
+      controlRPM();
+
+      if (rotationCount > NumberOfRotationsPerRun)
+      {
+        //Mark the run as complete after 50 rotations
+        runComplete = true;
+        //turn off the motor
+        esc.writeMicroseconds(ESCMin);
+
+        //Publish results
+        int32_t accelerationSum = 0;
+        int32_t positionSum = 0;
+        for (int16_t i = 0; i < NumberOfRotationsPerRun; i++)
+        {
+          accelerationSum += runData[0][i];
+          positionSum += runData[1][i];
+        }
+
+        Serial.println("$$$$$$$$$$$$$$$$$ End of Run $$$$$$$$$$$$$$$$$$$");
+        Serial.print("Avg Accel");
+        Serial.println((float_t)accelerationSum / NumberOfRotationsPerRun);
+        Serial.print("Avg Position");
+        Serial.println((float_t)positionSum / NumberOfRotationsPerRun);
+      }
+      rotationCompleteFlag = false;
+    }
+  }
+}
+
+void outputData()
+{
+  uint16_t dt = micros() - previousDataOutputMicros;
+  if (dt > dataInterval)
+  {
+    int32_t tempAccel = processMPU();
+    int32_t tempPosition = getPosition();
+    String data;
+    data.concat(currentRPM);
+    data.concat(",");
+    data.concat(tempAccel);
+    data.concat(",");
+    data.concat(tempPosition);
+    Serial.println(data);
+    if (maxAccelPerRotation < tempAccel)
+    {
+      maxAccelPerRotation = tempAccel;
+      maxAccelPosition = tempPosition;
+    }
+    previousDataOutputMicros = micros();
+  }
+}
+
+uint16_t getPosition()
+{
+  uint16_t positionBasedonPulseCount;
+  uint16_t positionBasedonTime;
+  //2 parts, calculate the first portion based on the numbers of pulses and the degrees/pole
+  // 2nd portion is the time from the last pulse to the current time to give time since last pulse
+  //divide the time since the last pulse by a floating average pulse length to give the portion of the time complete for the current pulse cycle
+  //multiply the portion complete of the latest pulse by the degrees per pole to give the current position
+  positionBasedonPulseCount = (360 / numberOfPoles) * pulseCount;
+  positionBasedonTime = (micros() - lastPulseMicros) / averageTimePerPulseFromPreviousRevolution * degreesPerPole;
+
+  //Constrain the time based angle of the last pole to a maximum of the angle of a single pole
+  //Not really needed when the motor is running, but otherwise we get messy data when not running the motor
+  positionBasedonTime = constrain(positionBasedonTime, 0, 360 / numberOfPoles);
+
+  //Handle the angle offset to the zero position
+  return (positionBasedonPulseCount + positionBasedonTime + zeroAngleOffset) % 360;
+}
+
+void calcRPM()
+{
+  uint32_t dt = micros() - previousRotationMicros;
+
+  //Time for a single rotation divided into 60 to get RPM
+  uint16_t tempRPM = 60 / (dt / 1000000.00);
+
+  currentRPM = EMA_function(0.4, tempRPM, currentRPM);
+
+  previousRotationMicros = micros();
+}
+
+void controlRPM()
+{
+  static int32_t cumulativeError;
+  static int32_t previousError;
+  float rateError;
+  int32_t error;
+
+  uint32_t dt = micros() - previousRPMControllerMicros;
+
+  if (dt > RPMControllerInterval)
+  {
+
+    error = targetRPM - currentRPM;
+
+#ifdef DEBUG
+    Serial.print("Error: ");
+    Serial.println(error);
+#endif
+
+    rateError = (float)(error - previousError) / dt;
+
+    cumulativeError += error * dt;
+
+    outputESC = kP * error + kI * cumulativeError + kD * rateError;
+
+#ifdef DEBUG
+    Serial.print("Raw Output: ");
+    Serial.println(outputESC);
+#endif
+
+    //Map the PID output which can be negative from 0 to the ESC min to the range limiter which is our control authority range (1000-~1300us)
+    outputESC = map(outputESC, -5000, 5000, 0, ESCRangeLimiter - ESCMin);
+
+    //Add in the offset from 0 to 1000 which is min throttle
+    outputESC += ESCMin;
+
+    //Make sure the ESC doesn't recieve a signal it can't use and cap the upper limit
+    outputESC = constrain(outputESC, ESCMin, ESCRangeLimiter);
+#ifdef DEBUG
+    Serial.print("Mapped Output: ");
+    Serial.println(outputESC);
+
+    Serial.print("Current RPM: ");
+    Serial.println(currentRPM);
+#endif
+
+    esc.writeMicroseconds(outputESC);
+
+    previousError = error;
+
+    previousRPMControllerMicros = micros();
+  }
+}
+
+void strobeLED()
+{
+  static boolean LEDOn;
+
+  if (strobeLEDFlag)
+  {
+    // Serial.println("Begin Strobe");
+    //Turn on Strobe LED
+    digitalWrite(LEDPin, HIGH);
+    //Start timer for strobe duration
+    strobeLEDPreviousMicros = micros();
+    LEDOn = true;
+    strobeLEDFlag = false;
+  }
+
+  uint32_t dt = micros() - strobeLEDPreviousMicros;
+  if (dt > strobeLEDDuration && LEDOn)
+  {
+    // Serial.println("End Strobe");
+    //Turn off the strobe LED when the duration is complete and reset the strobe flag
+    LEDOn = false;
+    digitalWrite(LEDPin, LOW);
+  }
+}
+
+float_t processMPU()
+{
+
+  float_t y_accel;
+  y_accel = mpu.getAccelerationY();
+
+#ifdef Debug
+  Serial.println(y_accel);
+#endif
+
+  return y_accel;
+}
+
+float EMA_function(float alpha, float latest, float stored)
+{
+  return (alpha * latest) + ((1 - alpha) * stored);
+}
